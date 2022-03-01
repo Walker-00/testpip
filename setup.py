28bf49dcf4e8dac3b56b9f63b44469a2369fa2e9
Walker-00 <85013114+Walker-00@users.noreply.github.com>
2022-03-01 11:02:30 +0630

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'TestPiP By WALKER'
+LONG_DESCRIPTION = 'TEST'
+
+# Setting up
+setup(
+    name="testpip",
+    version=VERSION,
+    author="W4lk3r",
+    author_email="<w000alker@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords=['requests'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
